f00e7085e9a24ea0c72bad1d83dc684fec05f688
ubourdon <bourdon.ugo@gmail.com>
2023-04-12 13:29:13 +0200

    step-3

--- src/main/scala/fp/scala/app/models/ApiResults.scala
+++
@@ -1,6 +0,0 @@
-package fp.scala.app.models
-
-import fp.scala.utils.models.safeuuid.SafeUUID
-
-object ApiResults:
-	case class CRUDResult(uid: SafeUUID)
--- src/main/scala/fp/scala/uno/service/CommandHandlerError.scala
+++
@@ -1,8 +0,0 @@
-package fp.scala.uno.service
-
-import fp.scala.app.infrastructure.models.DbError
-import fp.scala.uno.domain.UnoErreur
-
-enum CommandHandlerError:
-	case DomainError(error: UnoErreur) extends CommandHandlerError
-	case FromDbError(e: DbError) extends CommandHandlerError
